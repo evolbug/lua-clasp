52dae5035ae10631bb4af507ab5d48f966b1a710
Daniels Kursits <daniels.kursits@gmail.com>
2017-04-24 00:03:24 +0300

    forgot local

--- clasp.lua
+++ clasp.lua
@@ -1,12 +1,12 @@
 -- evolbug 2017, MIT License
 -- clasp - class library
 
-function new(self, ...)
+local function new(self, ...)
    local meta = {__index=self}
    for k,v in pairs(self.__ or {}) do meta['__'..k]=v end
    local object = setmetatable({}, meta)
    return object.init and object:init(...) and object or object
 end
-function extend(base, members) return setmetatable(members, {__index=base,__call=new}) end
+local function extend(base, members) return setmetatable(members, {__index=base,__call=new}) end
 return function(members) return extend({new=new, extend=extend}, members or {}) end
 

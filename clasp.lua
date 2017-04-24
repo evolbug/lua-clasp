66055ff134fa24b0eaad9b93e18564ed2797adee
Daniels Kursits <daniels.kursits@gmail.com>
2017-04-24 08:23:16 +0300

    blank class declaration fix; instance creation side effects

--- clasp.lua
+++ clasp.lua
@@ -5,8 +5,7 @@ function new(self, ...)
    local meta = {__index=self}
    for k,v in pairs(self.__ or {}) do meta['__'..k]=v end
    local object = setmetatable({}, meta)
-   return object.init and object:init(...) and object or object
+   return object, object.init and object:init(...)
 end
-function extend(base, members) return setmetatable(members, {__index=base,__call=new}) end
-return function(members) return extend({new=new, extend=extend}, members or {}) end
-
+function extend(base, members) return setmetatable(members or {}, {__index=base,__call=new}) end
+return function(members) return extend({new=new, extend=extend}, members) end

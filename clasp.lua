fdaef77a303a8f82a179e2cb0d99c48eac2ae736
Daniels Kursits <daniels.kursits@gmail.com>
2017-04-25 22:06:19 +0300

    fix when argument `proto` is not a table

--- clasp.lua
+++ clasp.lua
@@ -1,9 +1,7 @@
 -- evolbug 2017, MIT License
 -- clasp - class library
 
-local class = { init = function()end; extend = function(self, proto)
-    local meta = {__index = proto}
-    local proto = setmetatable(proto or {},{__index=self, __call=function(_, ...) local o=setmetatable({},meta) return o,o:init(...) end})
-    for k,v in pairs(proto.__ or {}) do meta['__'..k]=v end
-    return proto end }
+local class = { init = function()end; extend = function(self, proto) local meta = {}
+    local proto = setmetatable(proto or {},{__index=self, __call=function(_,...) local o=setmetatable({},meta) return o,o:init(...) end})
+    meta.__index = proto ; for k,v in pairs(proto.__ or {}) do meta['__'..k]=v end ; return proto end }
 return setmetatable(class, { __call = class.extend })
